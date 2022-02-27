4c3a0d42ba587693dd3d31d0f073dbdcac90bad3
Ruslan Ishmukhametov <ruslan@k7tech.agency>
2022-02-27 23:42:33 +0300

    Initial commit. All task requirements are done

---
+++ hello.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>PHP Test</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <img class="logo" src="./logo.png" alt="Mospolytech Logo"/>
+    <p>Hello, World!</p>
+</header>
+<main>
+    <?= '<h1>Hello World</h1>' ?>
+</main>
+<footer>
+    Задание для самостоятельно работы
+</footer>
+</body>
+</html>
